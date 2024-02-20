35cb503461ca1b0698a9540ff3656c9cee9bad0f
rekebuba <abukiabdullahi1@gmail.com>
2024-02-20 19:40:36 +0300

    sorting_algorithms

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -10,7 +10,7 @@
  */
 void quick_sort_hoare(int *array, size_t size)
 {
-    quick_sort_rec(array, size, 0, size - 1);
+	quick_sort_rec(array, size, 0, size - 1);
 }
 
 /**
@@ -22,15 +22,15 @@ void quick_sort_hoare(int *array, size_t size)
  */
 void swap(int *a, int *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 /**
  * quick_sort_rec - a recursion function that sorts
  * an array by using divide a concur method
- * 
+ *
  * @array: the given array to sort
  * @size: the size of the array
  * @left: the lowest index to the array
@@ -39,12 +39,13 @@ void swap(int *a, int *b)
  */
 void quick_sort_rec(int *array, size_t size, size_t left, size_t right)
 {
-    if (left < right)
-    {
-        int j = pivot(array, size, left, right);
-        quick_sort_rec(array, size, left, j);
-        quick_sort_rec(array, size, j + 1, right);
-    }
+	if (left < right)
+	{
+		int j = pivot(array, size, left, right);
+
+		quick_sort_rec(array, size, left, j);
+		quick_sort_rec(array, size, j + 1, right);
+	}
 }
 
 /**
@@ -58,25 +59,26 @@ void quick_sort_rec(int *array, size_t size, size_t left, size_t right)
  */
 int pivot(int *array, size_t size, size_t left, size_t right)
 {
-    int pivot_value = array[right];
-    size_t i = left, j = right;
-    while (i < j)
-    {
-        while (array[i] <= pivot_value)
-        {
-            i++;
-        }
-        while (array[j] > pivot_value)
-        {
-            j--;
-        }
-        if (i < j)
-        {
-            swap(&array[i], &array[j]);
-            print_array(array, size);
-        }
-    }
-    if (j == right)
-        j--;
-    return (j);
+	int pivot_value = array[right];
+	size_t i = left, j = right;
+
+	while (i < j)
+	{
+		while (array[i] <= pivot_value)
+		{
+			i++;
+		}
+		while (array[j] > pivot_value)
+		{
+			j--;
+		}
+		if (i < j)
+		{
+			swap(&array[i], &array[j]);
+			print_array(array, size);
+		}
+	}
+	if (j == right)
+		j--;
+	return (j);
 }
