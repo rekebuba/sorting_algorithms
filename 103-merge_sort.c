659b5976c2f0bf9d7a32754ca12e685c9ddbbc8d
rekebuba <abukiabdullahi1@gmail.com>
2024-02-20 23:57:14 +0300

    sorting_algorithms

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,70 +1,77 @@
 #include "sort.h"
 
 /**
- * merge_sort - sorts an array of integers in ascending order
- * using the Merge sort algorithm
+ * merge_list - Sort a sub array of integers.
+ * @sub_array: A sub array of an array of integers to sort.
+ * @buff: A buffer to store the sorted sub array.
+ * @left: The left index of the array.
+ * @mid: The middle index of the array.
+ * @right: The right index of the array.
+ */
+void merge_list(int *sub_array, int *buff, size_t left, size_t mid,
+		size_t right)
+{
+	size_t i, j, k = 0;
+
+	printf("Merging...\n[left]: ");
+	print_array(sub_array + left, mid - left);
+
+	printf("[right]: ");
+	print_array(sub_array + mid, right - mid);
+
+	for (i = left, j = mid; i < mid && j < right; k++)
+		buff[k] = (sub_array[i] < sub_array[j]) ? sub_array[i++] : sub_array[j++];
+	for (; i < mid; i++)
+		buff[k++] = sub_array[i];
+	for (; j < right; j++)
+		buff[k++] = sub_array[j];
+	for (i = left, k = 0; i < right; i++)
+		sub_array[i] = buff[k++];
+
+	printf("[Done]: ");
+	print_array(sub_array + left, right - left);
+}
+
+/**
+ * divide_list - Implement the merge sort algorithm through recursion.
+ * @sub_array: A sub array of an array of integers to sort.
+ * @buff: A buffer to store the sorted result.
+ * @left: The left index of the sub array.
+ * @right: The right index of the sub array.
+ */
+void divide_list(int *sub_array, int *buff, size_t left, size_t right)
+{
+	size_t mid;
+
+	if (right - left > 1)
+	{
+		mid = left + (right - left) / 2;
+		divide_list(sub_array, buff, left, mid);
+		divide_list(sub_array, buff, mid, right);
+		merge_list(sub_array, buff, left, mid, right);
+	}
+}
+
+/**
+ * merge_sort - Sort an array of integers in ascending
+ *              order using the merge sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
  *
- * @array: the given array
- * @size: the size of the array
- * Return: Void
+ * Description: Implements the top-down merge sort algorithm.
  */
 void merge_sort(int *array, size_t size)
 {
-	divide_list(array, 0, size - 1);
-}
+	int *buff;
 
-/**
- * divide_list - divide the array and sort them
- *
- * @array: the array passed
- * @lower: the lower bound
- * @upper: the upper bound
- * Return: Void
- */
-void divide_list(int *array, size_t lower, size_t upper)
-{
-	size_t mid = (lower + upper) / 2;
+	if (array == NULL || size < 2)
+		return;
 
-	divide_list(array, lower, mid);
-	divide_list(array, mid + 1, upper);
-	merge_list(array, lower, mid, upper);
-}
+	buff = malloc(sizeof(int) * size);
+	if (buff == NULL)
+		return;
 
-/**
- * merge_list - apply the merge algorithm
- *
- * @array: the array passed
- * @lower: the lower bound
- * @mid: the middle value
- * @upper: the upper bound
- */
-void merge_list(int *array, size_t lower, size_t mid, size_t upper)
-{
-	size_t index = 0, i = lower, j = mid;
-	int *result = malloc(upper * sizeof(int));
+	divide_list(array, buff, 0, size);
 
-	while (i <= mid && j <= upper)
-	{
-		if (array[i] >= array[j])
-		{
-			result[index++] = array[j];
-			j++;
-		}
-		else
-		{
-			result[index++] = array[i];
-			i++;
-		}
-	}
-	for (; j <= upper; j++)
-	{
-		result[index++] = array[j];
-	}
-	for (; i <= mid; i++)
-	{
-		result[index++] = array[i];
-	}
-
-	for (i = mid, j = 0; j < index; i++, j--)
-		array[i] = result[j];
+	free(buff);
 }
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,2 @@
+
+    printf("[Done]: ");
